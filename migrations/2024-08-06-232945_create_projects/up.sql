122f23660686352ce40e8ad4d45e0f1e4b51426b
Thomas Derudder <thomasderudder@sfr.fr>
2024-08-07 12:39:31 +0200

    migrations of projects

---
+++ migrations/2024-08-06-232945_create_projects/up.sql
@@ -0,0 +1,15 @@
+-- Your SQL goes here
+CREATE TABLE projects (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+CREATE TABLE user_projects (
+  user_id INT NOT NULL,
+  project_id INT NOT NULL,
+  PRIMARY KEY (user_id, project_id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (project_id) REFERENCES projects(id)
+);
