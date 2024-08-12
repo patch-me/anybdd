228573c795eb285e81e3bdbba0d63d37e269a9e4
Thomas Derudder <thomasderudder@sfr.fr>
2024-08-12 11:38:42 +0200

    correction task

--- migrations/2024-08-07-103410_create_tasks/up.sql
+++ migrations/2024-08-07-103410_create_tasks/up.sql
@@ -14,3 +14,10 @@ CREATE TABLE project_tasks (
   FOREIGN KEY (project_id) REFERENCES projects(id),
   FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
+CREATE TABLE user_tasks (
+  user_id INT NOT NULL,
+  task_id INT NOT NULL,
+  PRIMARY KEY (user_id, task_id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (task_id) REFERENCES tasks(id)
+);
