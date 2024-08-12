cdf81dab433186b4e9cec9f301dbbe774ec8a44f
Thomas Derudder <thomasderudder@sfr.fr>
2024-08-12 14:40:41 +0200

    Add result and error

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,30 @@ use diesel::mysql::MysqlConnection;
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use models::Project;
+use std::error::Error;
 use std::env;
+use crate::schema::projects::dsl::*; // Import the `projects` table and associated DSL items
 
-fn main() {
-    use self::schema::projects::dsl::*;
-    let connection: &mut MysqlConnection = &mut establish_connection();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut connection = establish_connection()?; // Handle the Result and unwrap the MysqlConnection
 
     let results: Vec<Project> = projects
         .select(Project::as_select())
-        .load(connection)
+        .load(&mut connection)
         .expect("Error loading projects");
+    
     println!("{:?}", results);
 
+    Ok(())
 }
-pub fn establish_connection() -> MysqlConnection {
+
+pub fn establish_connection() -> Result<MysqlConnection, Box<dyn Error>> {
     dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    MysqlConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
+    let database_url = env::var("DATABASE_URL")?;
+    // Use ? instead of expect to propagate the error
+    let connection = MysqlConnection::establish(&database_url)
+        .map_err(|e| format!("Error connecting to {}: {}", database_url, e))?;
+    
+    Ok(connection)
 }
