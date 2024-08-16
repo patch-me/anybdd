705abaa9e5683e85c4f1832b4a2e428d738f7657
Ripitchip <thomasderudder@sfr.fr>
2024-08-16 17:01:48 +0000

    :sparkles: feat: #5 Correction Delete and patch trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ pub mod connection;
 pub mod models;
 pub mod schema;
 pub mod user;
+use std::usize;
+
 use diesel::mysql::MysqlConnection;
 use diesel::result::Error;
 
@@ -15,10 +17,10 @@ pub trait Post<T, U> {
 }
 
 pub trait Patch<T> {
-    fn edit(item: T) -> Result<(), Error>;
+    fn edit(connection: &mut MysqlConnection, item: T) -> Result<usize, Error>;
 }
 
 pub trait Delete<T> {
-    fn delete(item: T) -> Result<(), Error>;
-    fn delete_by_id(id: i32) -> Result<(), Error>;
+    fn delete(connection: &mut MysqlConnection, item: T) -> Result<usize, Error>;
+    fn delete_by_id(connection: &mut MysqlConnection, id: i32) -> Result<usize, Error>;
 }
