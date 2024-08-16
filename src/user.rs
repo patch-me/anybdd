cf5995038bbfdb0cfb5c683f2d62403fa7da8d63
Ripitchip <thomasderudder@sfr.fr>
2024-08-16 17:03:41 +0000

    :sparkles: feat: #5 Configuration user for Patch and Delete

--- src/user.rs
+++ src/user.rs
@@ -1,8 +1,9 @@
 use crate::models::{NewUser, User};
+use crate::schema::users;
 use crate::{Delete, Get, Patch, Post};
 use diesel::mysql::MysqlConnection;
 use diesel::result::Error;
-use diesel::{insert_into, prelude::*};
+use diesel::{connection, delete, insert_into, prelude::*, update};
 
 struct UserService;
 
@@ -24,7 +25,24 @@ impl Get<User> for UserService {
 impl Post<User, NewUser> for UserService {
     fn create(connection: &mut MysqlConnection, item: NewUser) -> Result<usize, Error> {
         use crate::schema::users;
-
         insert_into(users::table).values(item).execute(connection)
+    }
+}
+impl Patch<User> for UserService {
+    fn edit(connection: &mut MysqlConnection, item: User) -> Result<usize, Error> {
+        use crate::schema::users;
+        update(users::table.find(item.id))
+            .set(item)
+            .execute(connection)
+    }
+}
+impl Delete<User> for UserService {
+    fn delete(connection: &mut MysqlConnection, item: User) -> Result<usize, Error> {
+        delete(users::table.find(item.id)).execute(connection)
+    }
+    fn delete_by_id(connection: &mut MysqlConnection, id: i32) -> Result<usize, Error> {
+        delete(users::table)
+            .filter(users::id.eq(id))
+            .execute(connection)
     }
 }
