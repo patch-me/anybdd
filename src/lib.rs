f18d43c387d5a78d2989addd1276cfcfcd8f7128
Ripitchip <thomasderudder@sfr.fr>
2024-08-16 08:58:47 +0000

    :sparkles: feat: #5 moving connection function position

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,4 @@
+pub mod connection;
 pub mod models;
 pub mod schema;
-use diesel::mysql::MysqlConnection;
-use diesel::prelude::*;
-use dotenvy::dotenv;
-use std::env;
-
-pub fn establish_connection() -> MysqlConnection {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    MysqlConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
-}
+pub mod user;
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,18 @@
-pub mod models;
-pub mod schema;
-use diesel::mysql::MysqlConnection;
+use anybdd::connection::establish_connection;
+use anybdd::models::Project;
+use anybdd::schema::projects::dsl::*;
 use diesel::prelude::*;
-use dotenvy::dotenv;
-use models::Project;
 use std::error::Error;
-use std::env;
-use crate::schema::projects::dsl::*; // Import the `projects` table and associated DSL items
-
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut connection = establish_connection()?; // Handle the Result and unwrap the MysqlConnection
+    let mut connection = establish_connection();
 
     let results: Vec<Project> = projects
         .select(Project::as_select())
         .load(&mut connection)
         .expect("Error loading projects");
-    
+
     println!("{:?}", results);
 
     Ok(())
-}
-
-pub fn establish_connection() -> Result<MysqlConnection, Box<dyn Error>> {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL")?;
-    // Use ? instead of expect to propagate the error
-    let connection = MysqlConnection::establish(&database_url)
-        .map_err(|e| format!("Error connecting to {}: {}", database_url, e))?;
-    
-    Ok(connection)
 }
