a4e86938d6cca6c5c21df18a1c9f519593f63c52
Thomas Derudder <thomasderudder@sfr.fr>
2024-09-04 22:32:49 +0200

    :sparkles: feat: #5 Creation of pool for connection

--- src/connection.rs
+++ src/connection.rs
@@ -1,11 +1,15 @@
 use diesel::mysql::MysqlConnection;
-use diesel::prelude::*;
+use diesel::r2d2::ConnectionManager;
+use diesel::r2d2::Pool;
 use dotenvy::dotenv;
 use std::env;
 
-pub fn establish_connection() -> MysqlConnection {
+pub fn get_connection_pool() -> Pool<ConnectionManager<MysqlConnection>> {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    MysqlConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
+    let manager = ConnectionManager::<MysqlConnection>::new(database_url);
+    Pool::builder()
+        .test_on_check_out(true)
+        .build(manager)
+        .expect("Could not build connection pool")
 }
