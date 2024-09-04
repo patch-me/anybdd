370a163c1df1c2a1b1bb1927e50e9d846022f6a5
Thomas Derudder <thomasderudder@sfr.fr>
2024-09-05 00:17:01 +0200

    :sparkles: feat: #8 Cleaning and using crud method

--- src/lib.rs
+++ src/lib.rs
@@ -5,35 +5,25 @@ pub mod schema;
 pub mod tag;
 pub mod task;
 pub mod user;
-pub mod user_project;
-use std::usize;
 
 use diesel::mysql::MysqlConnection;
 use diesel::result::Error;
 
-pub trait Get<T> {
-    fn get(connection: &mut MysqlConnection) -> Result<Vec<T>, Error>;
-    fn get_by_id(connection: &mut MysqlConnection, id: i32) -> Result<Vec<T>, Error>;
-}
-pub trait GetTools<T, U>: Get<T> {
-    fn get_by_info(connection: &mut MysqlConnection, data: U) -> Result<T, Error>;
+pub trait Read {
+    fn get(connection: &mut MysqlConnection, value: &i32) -> Result<Box<Self>, Error>;
 }
 
-pub trait Post<T> {
-    fn create(connection: &mut MysqlConnection, item: T) -> Result<usize, Error>;
+pub trait Create {
+    fn create(connection: &mut MysqlConnection, item: &i32) -> Result<bool, Error>;
 }
 
-pub trait PostWithExtra<T> {
-    fn create(connection: &mut MysqlConnection, item: T, extra: Vec<i32>) -> Result<usize, Error>;
+pub trait Update {
+    fn update(connection: &mut MysqlConnection, item: &i32) -> Result<bool, Error>;
 }
 
-pub trait Patch<T> {
-    fn edit(connection: &mut MysqlConnection, item: T) -> Result<usize, Error>;
+pub trait Delete {
+    fn delete(connection: &mut MysqlConnection, item: &i32) -> Result<bool, Error>;
 }
-
-pub trait Delete<T> {
-    fn delete(connection: &mut MysqlConnection, item: T) -> Result<usize, Error>;
-}
-pub trait DeleteTools<T>: Delete<T> {
-    fn delete_by_id(connection: &mut MysqlConnection, id: i32) -> Result<usize, Error>;
+pub trait List {
+    fn list(connection: &mut MysqlConnection, item: &i32) -> Result<bool, Error>;
 }
