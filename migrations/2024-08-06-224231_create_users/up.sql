1876c042dc65606c83c3d755ab91cf963165674e
Thomas Derudder <thomasderudder@sfr.fr>
2024-09-05 19:37:05 +0200

    :sparkles: feat: #12 Add gitlab and github table

--- migrations/2024-08-06-224231_create_users/up.sql
+++ migrations/2024-08-06-224231_create_users/up.sql
@@ -6,3 +6,15 @@ CREATE TABLE users (
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+CREATE TABLE user_gitlab (
+  user_id INT NOT NULL,
+  gitlab_token VARCHAR(255) NOT NULL, -- Assuming the token is a string
+  PRIMARY KEY (user_id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+CREATE TABLE user_github (
+  user_id INT NOT NULL,
+  github_token VARCHAR(255) NOT NULL, -- Assuming the token is a string
+  PRIMARY KEY (user_id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
