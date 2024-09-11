7568a3ba4bd5f54012f195645a6ae59d02f44a8c
Thomas Derudder <thomasderudder@sfr.fr>
2024-09-11 23:40:35 +0200

    :sparkles: feat: #10 Create duplication error test

--- src/tests/user/create.rs
+++ src/tests/user/create.rs
@@ -7,7 +7,7 @@ use crate::connection::get_connection_pool;
 use crate::{models::user::NewUser, schema::users::dsl::*};
 
 #[test]
-fn test_create_user() {
+fn test_user_creation() {
   // Set up connection pool for the database (using Arc for thread safety)
   let pool = Arc::new(get_connection_pool());
 
@@ -23,6 +23,40 @@ fn test_create_user() {
   })
   .unwrap();
 
+  let count: i64 =
+    users.select(count(username)).first::<i64>(&mut connection).unwrap();
+
+  assert_eq!(count, 1);
+
+  diesel::delete(users).execute(&mut connection).unwrap();
+}
+
+#[test]
+fn test_user_duplication() {
+  // Set up connection pool for the database (using Arc for thread safety)
+  let pool = Arc::new(get_connection_pool());
+
+  // Get a connection from the pool
+  let mut connection =
+    pool.get().expect("Failed to get a connection from the pool.");
+
+  diesel::delete(users).execute(&mut connection).unwrap();
+  // // Create a new user object
+  NewUser::create(&mut connection, &NewUser {
+    username: String::from("john_doe"),
+    password: String::from("secure_password"),
+  })
+  .unwrap();
+  let result = NewUser::create(&mut connection, &NewUser {
+    username: String::from("john_doe"),
+    password: String::from("secure_password"),
+  });
+
+  // Assert that the result is an error
+  assert!(result.is_err(), "Expected an error, but got success: {:?}", result);
+  let pool = Arc::new(get_connection_pool());
+  let mut connection =
+    pool.get().expect("Failed to get a connection from the pool.");
   let count: i64 =
     users.select(count(username)).first::<i64>(&mut connection).unwrap();
 
