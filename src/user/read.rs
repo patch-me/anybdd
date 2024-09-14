444dd085fc80eab544098b7336e6b919a6917733
Thomas Derudder <thomasderudder@sfr.fr>
2024-09-15 01:01:55 +0200

    :sparkles: feat: #10 Add tests for user read

--- src/user/read.rs
+++ src/user/read.rs
@@ -1,22 +1,23 @@
+use diesel::{mysql::MysqlConnection, prelude::*, result::Error};
+
 use crate::models::user::User;
-use diesel::mysql::MysqlConnection;
-use diesel::prelude::*;
-use diesel::result::Error;
 
 impl User {
-    pub fn get(connection: &mut MysqlConnection, value: &i32) -> Result<Self, Error> {
-        // Implement logic to get a single user here
-        use crate::schema::users;
-        users::table
-            .filter(users::id.eq(value))
-            .first::<User>(connection)
-    }
+  pub fn get(
+    connection: &mut MysqlConnection,
+    value: &i32,
+  ) -> Result<Self, Error> {
+    // Implement logic to get a single user here
+    use crate::schema::users;
+    users::table.filter(users::id.eq(value)).first::<User>(connection)
+  }
 
-    fn get_by_name(connection: &mut MysqlConnection, value: &String) -> Result<Self, Error> {
-        // Implement logic to get all users by name here
-        use crate::schema::users;
-        users::table
-            .filter(users::username.eq(value))
-            .first::<User>(connection)
-    }
+  pub fn get_by_name(
+    connection: &mut MysqlConnection,
+    value: &String,
+  ) -> Result<Self, Error> {
+    // Implement logic to get all users by name here
+    use crate::schema::users;
+    users::table.filter(users::username.eq(value)).first::<User>(connection)
+  }
 }
